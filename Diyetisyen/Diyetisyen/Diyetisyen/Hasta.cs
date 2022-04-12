a596e57f93180fa6237eeea24c0ac63e4e720b1d
Aydın Can <81384606+aydinncann@users.noreply.github.com>
2022-04-12 16:39:44 +0300

    Add files via upload

---
+++ Diyetisyen/Diyetisyen/Diyetisyen/Hasta.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Diyetisyen
+{
+    public class Hasta
+    {
+        public int HastaKayitNo { get; set; }
+        public ulong Tc { get; set; }
+        public string HastaAdi { get; set; }
+        public string HastaSoyadi { get; set; }
+        public string HastalikAdi { get; set; }
+        public string DiyetAdi { get; set; }
+        public DateTime DiyetVerilisTarihi { get; set; }
+        //  public List<string> diyetTakvimi = new List<string>();
+       // public string[,] diyetTakvimi = new string[7, 3];
+    }
+}
